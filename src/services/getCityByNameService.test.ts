ba2cb725cdcd712e145eecb4dfe86c483ec60349
keflemTrindade <keflemtn@gmail.com>
2024-03-07 14:36:42 -0400

    test: add mocks

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -1,20 +1,9 @@
+import { mockCityApiResponse } from "@__tests__/mocks/mockCityApiResponse";
 import { api } from "./api";
 import { getCityByNameService } from "./getCityByNameService";
 describe("API: getCityByNameService", () => {
   it("should return city details", async () => {
-    const data = {
-      id: "1",
-      name: "Cuiabá",
-      sys: {
-        country: "BR",
-      },
-      coord: {
-        lat: 123,
-        lon: 456,
-      },
-    };
-
-    jest.spyOn(api, "get").mockResolvedValue({ data });
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityApiResponse });
     const response = await getCityByNameService("Cuiabá");
 
     expect(response.length).toBeGreaterThan(0);
